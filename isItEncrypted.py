e96957d8f7517745277fcacdc02a644714047d4f
orenIsabella <33618982+orenIsabella@users.noreply.github.com>
2019-05-05 15:14:33 +0000

    Create isItEncrypted.py
    This is the code that checks weather there was an attampt to encrypt or change anything in the folder that the user gives us.

---
+++ isItEncrypted.py
@@ -0,0 +1,34 @@
+import os
+
+def read_file(file):
+    with open(file) as f:
+        SMRF1 = f.readlines()
+    return SMRF1
+
+def itIsEncrypted(file):
+    print('someone is trying to encrypt your file:')
+    print(file)
+
+def isItEncrypted(file):
+    initial = read_file(file)
+    while True:
+        current = read_file(file)
+        if initial != current:
+            for line in current:
+                if line not in initial:
+                    itIsEncrypted(file)
+            #initial = current
+        current=initial
+
+
+path = input("Please enter the path of the folder you would like to protect: ")
+files = []
+# r=root, d=directories, f = files
+for r, d, files in os.walk(path):
+    for file in files:
+        if '.txt' in file:
+            files.append(os.path.join(r, file))
+while True:
+    for file in files:
+        isItEncrypted(file)
+        
